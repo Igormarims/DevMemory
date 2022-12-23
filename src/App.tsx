e5eb183d2cb70496ad87b5861f15b8be527f8f14
Igor marques <igormarquespe@gmail.com>
2022-12-23 16:07:12 -0300

    creating the button

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,37 @@
 import * as C from './App.styles';
 import LogoImage from '../src/assets/devmemory_logo.png';
 import  InfoItem  from './components/InfoItem';
+import Button from './components/Button';
+import Icon from './assets/restart.svg';
 
 function App() {
+
+
+
+
+
+  const resetAndCreateGrid = ()=> {
+
+  }
+
   return (
     <C.Container>
       <C.Info>
         <C.LogoLink href="">
-            <img src={ LogoImage } alt="" width={200} />
+            <img src={ LogoImage } alt="Logo" width={200} />
         </C.LogoLink>
          
         <C.InfoArea>
           <InfoItem label='Tempo'value='00:00'/>
           <InfoItem label='Movimentos'value='0'/>
         </C.InfoArea>
+        <Button label='Reiniciar' icon={Icon} onClick={resetAndCreateGrid}/>
 
-        <button>Reiniciar</button>
+        
        
       </C.Info>
      
       <C.GridArea>
-        ...
       </C.GridArea>
     </C.Container>
   );
