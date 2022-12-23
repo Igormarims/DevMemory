e1e436c152421c9d99c6e7f21980c651fa2a57fb
Igor marques <igormarquespe@gmail.com>
2022-12-23 14:35:35 -0300

    creating the structure

---
+++ src/App.styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+ 
+ export const  Container = styled.div `
+     width: 100%;
+     max-width:750px;
+     margin: auto;
+     display: flex;
+     padding: 50px 0;  
+     border: 1px solid red;
+
+     @media (max-width: 750px){
+        flex-direction: column;
+     }
+ `;
+
+ export const Info = styled.div `
+    display: flex;
+    flex-direction: column;
+    width: auto;
+    border: 1px solid red;
+     
+    @media (max-width: 750px){
+       margin-bottom: 50px;
+       align-items: center;
+    }
+
+ `;
+
+ export const LogoLink = styled.a `
+ border: 1px solid black;
+ 
+ `;
+
+ export const InfoArea = styled.div `
+ margin: 10px 0
+ width: 100%;
+ border: 1px solid green;
+
+ @media (max-width: 750px){
+   display: flex;
+   justify-content: space-around;
+   text-align: center;
+ }
+ `;
+
+ export const GridArea = styled.div `
+ flex: 1;
+ display: flex;
+ justify-content: flex-end;
+ border: 1px solid blue;
+
+ @media (max-width: 750px){
+    justify-content: center;
+    margin: 0 20px;
+ }
+ `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 
-import './App.css';
+import * as C from './App.styles';
+import LogoImage from '../src/assets/devmemory_logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+            <img src={ LogoImage } alt="" width={200} />
+        </C.LogoLink>
+         
+        <C.InfoArea>
+        ...
+        </C.InfoArea>
+
+        <button>Reiniciar</button>
+       
+      </C.Info>
      
-      </header>
-    </div>
+      <C.GridArea>
+        ...
+      </C.GridArea>
+    </C.Container>
   );
 }
 
